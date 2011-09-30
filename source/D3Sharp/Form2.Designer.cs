3aaa2eae77eeb2d34ee0e3e160beb40e215e776f
God601 <God601@gmail.com>
2011-09-29 20:00:11 -0700

    Edited comments

--- source/D3Sharp/Form2.Designer.cs
+++ source/D3Sharp/Form2.Designer.cs
@@ -28,28 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(36, 95);
+            this.label1.Location = new System.Drawing.Point(106, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(292, 64);
+            this.label1.Size = new System.Drawing.Size(251, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Je prend aucune responsabiliter sur ce projet.\r\nToute redistribution est totaleme" +
-                "nt interdite.\r\n\r\nD3GS par Karl Fortier-Champagne\r\n";
+            this.label1.Text = "Copyright (C) 2009-2011 The AllMightyOne\'s Project";
             // 
-            // About
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 65);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(367, 254);
+            this.ClientSize = new System.Drawing.Size(457, 169);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "About";
+            this.Name = "Form2";
             this.Text = "About";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -59,5 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
     }
 }
